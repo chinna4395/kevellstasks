fc43047e6c3bc00edc0d109ea7da34a6f32e3b97
chinna4395 <chinna4395@gmail.com>
2020-10-10 14:32:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface UserData {
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'kevellstasks';
+  ELEMENT_DATA:any =[];
+  displayedColumns: string[] = ['id', 'title', 'url', 'thumbnailUrl'];
+  dataSource: MatTableDataSource<UserData>;
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor( 
+                private http : HttpClient
+) {}
+
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource();
+    this.getLabs();
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator =this.paginator;
+    this.dataSource.paginator = this.paginator;
+
+  }
+
+  getLabs(){
+    this.http.get('https://jsonplaceholder.typicode.com/photos').subscribe((res)=>{
+      console.log(res);
+      this.ELEMENT_DATA = res;
+      this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+      console.log(this.dataSource);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+      return this.ELEMENT_DATA;
+    })
+  }
+
+
+applyFilter(event: Event) {
+  const filterValue = (event.target as HTMLInputElement).value;
+  this.dataSource.filter = filterValue.trim().toLowerCase();
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
+import { HttpClientModule } from "@angular/common/http";
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule,
+    HttpClientModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
